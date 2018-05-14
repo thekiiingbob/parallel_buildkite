08235ed72d4b6fc538e2e082b3c03f3b4f20c436
Bob Lubecker <blubecker@lifesize.com>
2018-05-14 09:29:58 -0500

    install docker-machine if not available

--- .buildkite/scripts/run_tests.sh
+++ .buildkite/scripts/run_tests.sh
@@ -43,7 +43,11 @@ then
   USE_DINGHY=true
   eval $(dinghy env)
 else
-  echo "Running on buildkite"
+  echo "Running on buildkite. Installing Docker machine"
+  # Install docker machine
+  base=https://github.com/docker/machine/releases/download/v0.14.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo install /tmp/docker-machine /usr/local/bin/docker-machine
 fi 
 
 # Create Docker Machine on AWS
